4ed8a975045875b160f4d0e6d493f2a805ad8751
Dascal Tiberiu Claudiu <tiberiu.purcel@gmail.com>
2020-04-10 00:43:16 +0300

    Finished templating

--- flaskblog.py
+++ flaskblog.py
@@ -1,38 +1,56 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '0b9b6caf316d00648849e522fefdc561'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
         'author': 'Corey Schafer',
-        'title': 'Blog post 1',
+        'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
         'author': 'Jane Doe',
-        'title': 'Blog post 2',
+        'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/register')
-def register():
-   form  =  RegistrationForm()
-   return render_template('register.html')
 
-if __name__ == "__main__":
-    app.run(debug=True, port=80)
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
